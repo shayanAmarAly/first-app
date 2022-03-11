500b8aba4090b7373f6c48635dd7863c5ad33a55
Shayan <shayanamaraly69@gmail.com>
2022-03-11 21:25:41 +0500

    Updates

---
+++ precache-manifest.bf03d6bcc8691a63a249c411f489c15c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0367bc753bb52de94a3ba1d47565773b",
+    "url": "/first-app/index.html"
+  },
+  {
+    "revision": "99c1965ff8e7e67d4594",
+    "url": "/first-app/static/css/main.16e18a48.chunk.css"
+  },
+  {
+    "revision": "1818aac688aeb0e601c0",
+    "url": "/first-app/static/js/2.48fafd73.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/first-app/static/js/2.48fafd73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71dfb25463a63b4e7042",
+    "url": "/first-app/static/js/3.0b270a1a.chunk.js"
+  },
+  {
+    "revision": "99c1965ff8e7e67d4594",
+    "url": "/first-app/static/js/main.d7001cbd.chunk.js"
+  },
+  {
+    "revision": "b39bc23d8fbd33169b9e",
+    "url": "/first-app/static/js/runtime-main.7913c23f.js"
+  }
+]);
